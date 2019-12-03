e36e777280515c8d71e12c7b5e81ded4e265c468
Amanda <49762182+sheehanab@users.noreply.github.com>
2019-12-03 09:18:44 -0500

    Update assignments.php

--- assignments.php
+++ assignments.php
@@ -2,6 +2,12 @@
 <?php
 	session_start();
 	require('dbconnect.php');
+
+	// Check if the user is logged in, if not then redirect to login page
+	if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    		header("location: login.php");
+    		exit;
+	}
 ?>
 <!DOCTYPE HTML>
 <html>
