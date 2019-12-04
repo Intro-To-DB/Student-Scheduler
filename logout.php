9c7a7ad3e978b452014da661bf3145477a6eb181
Amanda <49762182+sheehanab@users.noreply.github.com>
2019-12-04 18:43:08 -0500

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,10 +1,5 @@
 <!--log user out and remove session variables-->
 <?php
-	// Check if the user is logged in at all, if not then redirect to login page
-	if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-    		header("location: login.php");
-    		exit;
-	}
 
 	session_start();
 	session_unset();
